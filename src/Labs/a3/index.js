20962715837c2e5899ed81275f05b98e94ff442e
uzitak161 <77938678+uzitak161@users.noreply.github.com>
2023-10-11 13:10:44 -0400

    Started dynamic styling need to finish

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,10 +1,20 @@
 import JavaScript from "./JavaScript";
+import Nav from "../../Nav";
+import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+import DynamicStyling from "./DynamicStyling";
+
 function Assignment3() {
-    return (
-      <div>
-        <h1>Assignment 3</h1>
-        <JavaScript/>
-      </div>
-    );
-  }
-  export default Assignment3;
+  return (
+    <div>
+      <Nav />
+      <h1>Assignment 3</h1>
+      <Classes/>
+      
+      <JavaScript />
+      <PathParameters />
+      <DynamicStyling />
+    </div>
+  );
+}
+export default Assignment3;
