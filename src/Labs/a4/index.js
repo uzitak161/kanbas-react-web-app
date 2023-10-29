df7bcbb86634bd6a1b9f735784af2845a3d79229
Uzay Takil <uzaytakil01@gmail.com>
2023-10-29 19:12:40 -0400

    Lab exercises up to 2.4.2

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -3,6 +3,14 @@ import Add from "./Add";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariables";
+import ArrayStateVariable from "./ArrayStateVariables";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 function Assignment4 () {
   function sayHello() {
@@ -11,10 +19,18 @@ function Assignment4 () {
  return(
    <div>
      <h1>Assignment 4</h1>
+     <ReduxExamples />
      <Add a={1} b={2} />
      <ClickEvent />
      <PassingDataOnEvent />
      <PassingFunctions theFunction={sayHello} />
+     <Counter />
+     <BooleanStateVariables />
+     <StringStateVariables />
+     <DateStateVariable />
+     <ObjectStateVariable />
+     <ArrayStateVariable />
+     <ParentStateComponent />
    </div>
  );
 };
