9f172bfbffa6b2b1990cf5c64bc9e44ab8de6c99
Uzay Takil <uzaytakil01@gmail.com>
2023-10-29 20:56:27 -0400

    Finished lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,15 +1,23 @@
 import JavaScript from "./JavaScript";
-import Nav from "../../Nav";
 import PathParameters from "./PathParameters";
 import Classes from "./Classes";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todo/TodoList";
 import Styles from "./Styles";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
   return (
     <div>
       <h1>Assignment 3</h1>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
       <TodoList />
       <ConditionalOutput />
       <Styles />
--- src/Labs/a4/ReduxExamples/index.js
+++ src/Labs/a4/ReduxExamples/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import AddRedux from "./AddRedux";
+import CounterRedux from "./CounterRedux";
+import HelloRedux from "./HelloRedux";
+import TodoList from "./todos/TodoList";
 
 const ReduxExamples = () => {
   return(
     <div>
       <h2>Redux Examples</h2>
+      <TodoList />
+      <HelloRedux />
+      <CounterRedux />
+      <AddRedux />
     </div>
   );
 };
