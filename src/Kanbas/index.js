5ef59aefbb35e1110245344423b7740d11fb35a2
Uzay Takil <uzaytakil01@gmail.com>
2023-11-15 14:26:04 -0500

    Added FE API calls for fetching classes and posting new class

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -2,22 +2,38 @@ import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
-import db from "./Database";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 
 function Kanbas() {
 
-  const [courses, setCourses] = useState(db.courses);
+  const [courses, setCourses] = useState([]);
   const [course, setCourse] = useState({
     name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
   });
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+
+  const URL = "http://localhost:4000/api/courses";
+
+  const addNewCourse = async () => {
+    const response = await axios.post(URL, course);
+    setCourses([
+      response.data,
+      ...courses,
+    ])
+  }
+
+  const findAllCourses = async () => {
+    const response = await axios.get(URL);
+    setCourses(response.data);
   };
+  useEffect(() => {
+    findAllCourses();
+  }, []);
+
   const deleteCourse = (courseId) => {
     setCourses(courses.filter((course) => course._id !== courseId));
   };
